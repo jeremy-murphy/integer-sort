b89b53d8886f20f1f51ed9bb4e3e1a289058c2a9
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2013-07-19 21:16:21 +1000

    Threw out my first draft and adapted the version on Rosetta Code because it is 3 times faster. But is it stable???

--- counting-sort.hpp
+++ counting-sort.hpp
@@ -1,28 +1,50 @@
 #include <iterator>
 #include <algorithm>
+#include <cassert>
 
-/**
- * Requires that client allocates space for result beforehand.
- */
-
-template <typename ForwardIterator, typename ReverseIterator, typename OutputIterator>
-void counting_sort(ForwardIterator __first, ForwardIterator __last, ReverseIterator __rfirst, ReverseIterator __rlast, OutputIterator __result, unsigned const __k = 9)
+template <typename InputIterator>
+void fastcsort( InputIterator __first, InputIterator __last,
+                typename std::iterator_traits<InputIterator>::value_type const __max,
+                typename std::iterator_traits<InputIterator>::value_type const __min = 0)
 {
-    typedef typename std::iterator_traits<ForwardIterator>::value_type value_type;
+    typedef typename std::iterator_traits<InputIterator>::value_type value_type;
     
-    std::vector<value_type> C(__k + 1);
-    std::for_each(__first, __last, [&](value_type const e){ ++C[e]; });
+    assert(__first != __last);
+    assert(__max > __min);
+    
+    value_type const nlen = ( __max - __min ) + 1;
+    value_type temp[nlen];
+    std::fill_n((value_type*)(temp), nlen, value_type());
+    std::for_each(__first, __last, [&](value_type const e){ assert(__min <= e && e <= __max); temp[e - __min]++; });
+    
+    auto it(__first);
+    for( value_type i = __min; i <= __max; i++ )
+        while( temp[i - __min]-- )
+        {
+            *(it++) = i;
+        }
+}
 
-    /*
-    auto const __e(std::end(C));
-    for(auto __it(std::begin(C) + 1); __it != __e; ++__it)
-    {
-        *__it += *(__it - 1);
-    }
-    */
-    std::transform(std::begin(C) + 1, std::end(C), std::begin(C), std::begin(C) + 1, [](value_type const input1, value_type const input2){ return input1 + input2; });
-    std::for_each(__rfirst, __rlast, [&](value_type const e)
-    {
-        *(__result + --C[e]) = e;
-    });
+
+#include <cstring>
+#include <climits>
+
+
+template <typename T>
+void fastcsort( T* const arr, std::size_t const len, T const __max, T const __min = 0 )
+{
+    T z = 0;
+    T nlen = ( __max - __min ) + 1;
+    T temp[nlen];
+    memset( temp, 0, nlen * sizeof( T ) );
+    
+    for( std::size_t i = 0; i < len; i++ )
+        temp[arr[i] - __min]++;
+    
+    for( T i = __min; i <= __max; i++ )
+        while( temp[i - __min] )
+        {
+            arr[z++] = i;
+            temp[i - __min]--;
+        }
 }
--- main.cpp
+++ main.cpp
@@ -5,31 +5,41 @@
 #include <random>
 #include <functional>
 #include <algorithm>
+#include <chrono>
+#include <string>
 
 using namespace std;
 
-typedef unsigned T;
+typedef unsigned long T;
+
+// #define STRINGS
 
 int main(void)
 {
-    unsigned const k = 5;
+#ifndef STRINGS
+    T const k = 5, __min = 0;
+    unsigned const n = 10000000;
+    cout << "k = " << k << ", n = " << n << endl;
     mt19937 engine;
     uniform_int_distribution<T> dist(0, k);
     auto generator = bind(dist, engine);
-    vector<T> A = {2, 5, 3, 0, 2, 3, 0, 3};
-    cout << " Input: ";
-    for_each(begin(A), end(A), [](T const e){ cout << e << " "; });
-    cout << "\n";
-    /*
-    for(unsigned i = 0; i < 10; ++i)
-    {
+    vector<T> A;
+    A.reserve(n);
+    for(unsigned i = 0; i < n; ++i)
         A.push_back(generator());
-    }
-    */
-    vector<T> B(A.size());
-    counting_sort(A.cbegin(), A.cend(), A.crbegin(), A.crend(), begin(B), k);
-    cout << "Output: ";
-    for_each(begin(B), end(B), [](T const e){ cout << e << " "; });
-    cout << "\n";
+#else
+    string A("something wicked this way comes");
+    char const __min(*min_element(A.begin(), A.end())), k(*max_element(A.begin(), A.end()));
+    cout << "Before: " << A << endl;
+#endif
+    auto const t0(std::chrono::high_resolution_clock::now());
+    // counting_sort(A.cbegin(), A.cend(), A.crbegin(), A.crend(), begin(B), k);
+    fastcsort(A.begin(), A.end(), k, __min);
+    // sort(begin(A), end(A));
+    auto const t1(std::chrono::high_resolution_clock::now());
+    auto elapsed(chrono::duration_cast<chrono::milliseconds>(t1 - t0).count());
+    // cout << " After: " << A << endl;
+    cout << "Elapsed time: " << elapsed << " ms.\n";
+    
     return 0;
 }
