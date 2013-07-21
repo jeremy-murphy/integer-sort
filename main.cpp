9c82b9dc945ce1e83aab3fba24f9e6bc3aa346cf
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2013-07-21 21:22:30 +1000

    Add license stuff.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+//  (C) Copyright Jeremy W. Murphy 2013.
+//  Use, modification and distribution are subject to the 
+//  Boost Software License, Version 1.0. (See accompanying file 
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #include "boost/algorithm/sorting/integer_sort.hpp"
 
 #include "counting-sort.hpp"
