957074ca7c639becd27946b8341b1f53526211fa
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2013-08-02 15:51:23 +1000

    stable_radix_sort(): Don't sort empty ranges.

--- boost/algorithm/integer_sort/radix-sort.hpp
+++ boost/algorithm/integer_sort/radix-sort.hpp
@@ -35,47 +35,49 @@ namespace boost
         assert(_k >= _min);
         
         // Calculate some useful values.
-        difference_type const n(std::distance(_first, _last));
-        unsigned const b(sizeof(value_type) * 8);
         
-        /* TODO: Calculate and use effective b.
-         * It's assumed that value_type is no larger than necessary.  That is, k >= 2^(b/2).
-         * However, if k << 2^b, then there are a lot of unused significant bits that do not need sorting.
-         * Using effective b could reduce the number of passes required to sort.
-         */
-        
-        unsigned const flgn(floor(log2(n))); // TODO: Is there a faster way to calculate integer log2?
-        unsigned const r(_radix == 0 ? std::min((b < flgn ? b : flgn), 16u) : _radix); // Limit ourselves to sane values.
-        unsigned const d(ceil(static_cast<float>(b) / static_cast<float>(r)));
-        
-        // std::cout << "n = " << n << ", " << "b = " << b << ", " << "⌊lg(n)⌋ = " << flgn << ", r = " << r << ", d = " << d << std::endl;
-        assert(r * d >= b);
-        
-        if(d == 1)
+        if(_first != _last)
         {
-            boost::stable_counting_sort(_first, _last, _result, _k, _min);
-        }
-        else
-        {
-            std::vector<value_type> _input(_first, _last);
-            value_type const _dk = (value_type(1) << r) - 1; // TODO: This can be improved.
-            // NOTE: Is there an easy way to utilize _min here?
+            difference_type const n(std::distance(_first, _last));
+            unsigned const b(sizeof(value_type) * 8);
             
-            if(d == 2)
-            {
-                boost::stable_counting_sort(_first, _last, _input.begin(), _dk, 0, r, 0);
-                boost::stable_counting_sort(_input.begin(), _input.end(), _result, _dk, 0, r, 1);
-            }
+            /* TODO: Calculate and use effective b.
+            * It's assumed that value_type is no larger than necessary.  That is, k >= 2^(b/2).
+            * However, if k << 2^b, then there are a lot of unused significant bits that do not need sorting.
+            * Using effective b could reduce the number of passes required to sort.
+            */
+            
+            unsigned const _flgn(floor(log2(n))); // TODO: Is there a faster way to calculate integer log2?
+            unsigned const r(_radix == 0 ? std::min((b < _flgn ? b : _flgn), 16u) : _radix); // Limit ourselves to sane values.
+            unsigned const d(ceil(static_cast<float>(b) / static_cast<float>(r)));
+            
+            // std::cout << "n = " << n << ", " << "b = " << b << ", " << "⌊lg(n)⌋ = " << flgn << ", r = " << r << ", d = " << d << std::endl;
+            assert(r * d >= b);
+            
+            if(d == 1)
+                boost::stable_counting_sort(_first, _last, _result, _k, _min);
             else
             {
-                std::vector<value_type> _output(n);
-                for(unsigned _i = 0; _i < d; _i++)
-                {
-                    boost::stable_counting_sort(_input.begin(), _input.end(), _output.begin(), _dk, 0, r, _i);
-                    std::swap(_input, _output);
-                }
+                std::vector<value_type> _input(_first, _last);
+                value_type const _dk = (value_type(1) << r) - 1; // TODO: This can be improved.
+                // NOTE: Is there an easy way to utilize _min here?
                 
-                std::copy(_input.begin(), _input.end(), _result);
+                if(d == 2)
+                {
+                    boost::stable_counting_sort(_first, _last, _input.begin(), _dk, 0, r, 0);
+                    boost::stable_counting_sort(_input.begin(), _input.end(), _result, _dk, 0, r, 1);
+                }
+                else
+                {
+                    std::vector<value_type> _output(n);
+                    for(unsigned _i = 0; _i < d; _i++)
+                    {
+                        boost::stable_counting_sort(_input.begin(), _input.end(), _output.begin(), _dk, 0, r, _i);
+                        std::swap(_input, _output);
+                    }
+                    
+                    std::copy(_input.begin(), _input.end(), _result);
+                }
             }
         }
     }
